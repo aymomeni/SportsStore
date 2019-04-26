01ceac527f7781e87b4123cc3e3b6e98bc1a365e
aymomeni <aymomeni@gmail.com>
2019-04-26 12:49:18 -0600

    set up startup.cs & gitignore

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -15,20 +15,29 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage(); // details are shown when exception occurs during development phase
+            app.UseStatusCodePages(); // adds imple message to HTTP responses
+            app.UseStaticFiles(); // enables serving static files from the wwwroot folder
+            app.UseMvc(routes => // enables ASP.NET Core MVC
             {
-                app.UseDeveloperExceptionPage();
-            }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
             });
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
