a2db97ced8c511dc1ea4462afe42646c2e33921c
aymomeni <aymomeni@gmail.com>
2019-04-27 21:28:45 -0600

    added migration for EF inside .csproj and added db connection string

---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    /* database context class is the bridge between the application and Entity Framework Core and provides access 
+        to the application's data using model objects */
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
